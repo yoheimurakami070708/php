2f2dde9bf3208f5c73e4462f82ed9e9bd5769565
むらかみようへい <>
2021-08-18 21:30:56 +0900

    グラフの作成

--- login_app/resources/views/home.blade.php
+++ login_app/resources/views/home.blade.php
@@ -228,29 +228,28 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
 <script>
-  let mydate = new Date();
-  let Ye = mydate.getFullYear();
-  let Mo = mydate.getMonth() + 1;
-  let Da = mydate.getDate();
-  let Da1 = mydate.setDate(mydate.getDate() + 1);
-  let day = Ye + "年" + Mo + "月" + Da + "日";
-  let day1 = Ye + "年" + Mo + "月" + Da1 + "日";
+    
+    let day1 = moment().add('days', 1).format("YYYY年MM月DD日");
+    let day2 = moment().add('days', 2).format("YYYY年MM月DD日");
+    let day3 = moment().add('days', 3).format("YYYY年MM月DD日");
+    let day4 = moment().add('days', 4).format("YYYY年MM月DD日");
 
-  // var type = 'line'; 
-  // var data = { labels: [day, 2011, 2012, 2013], datasets: [{ label: '経験値', data: [120, 300, 200, 210] }, ] }; 
-  // var options; 
-  const ctx = document.getElementById('my_chart');
-  const chart_cv = new Chart(ctx, {
-    type: 'line', // グラフの種類
-    data: {
-      datasets: [{
-        label: '経験値',
-        data: [0, 120, 300, 800, 210]
-      }],
-      labels: ['', day, day1, 2012, 2013],
-    }
-  })
+    const ctx = document.getElementById('my_chart');
+        const chart_cv = new Chart(ctx, {
+          type: 'line', // グラフの種類
+          data: {
+              datasets: [
+                  {
+                      label: '経験値',
+                      data: [0,120, 300, 200, 210] 
+                  }
+              ],
+              labels: ['', day1, day2, day3, day4],
+          }
+        })
+
 </script>
 <script src="{{ asset('/js/home.blade.js')}}"></script>
 @endsection
