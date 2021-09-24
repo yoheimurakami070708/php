8b2ff1d3537036c28ba23dd924d8bfd255b5e0f1
yuhimizumura <awphack@gmail.com>
2021-09-24 11:00:44 +0900

    booststrap4の競合が理由でドロップダウンメニューが正しく動作しなかったため修正

--- login_app/resources/views/home.blade.php
+++ login_app/resources/views/home.blade.php
@@ -144,7 +144,7 @@
                 </td>
                 <!-- いいね機能 -->
                 <td class="align-middle ">
-                  @if ($user_id ?? '' == $score -> user_id )
+                  @if ($user_id == $score -> user_id )
                   <button class="bg-transparent border-0 fav text-danger" data-id="data{{$score -> id}}" value="{{$score -> id}}"><i class="fas fa-heart"></i></button>
                   @else
                   <button class="bg-transparent border-0 fav" data-id="data{{$score -> id}}" value="{{$score -> id}}"><i class="fas fa-heart"></i></button>
@@ -191,9 +191,9 @@
   </div>
 </div>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+{{--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>--}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+{{--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>--}}
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
 
